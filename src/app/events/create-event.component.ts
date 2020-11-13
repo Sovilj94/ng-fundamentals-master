d3458fbda708ce2a1a2d0cee8d5652391c52c923
Nikola Sovilj <nikola.sovilj@lanaco.com>
2020-11-13 22:51:02 +0100

    first commit

---
+++ src/app/events/create-event.component.ts
@@ -0,0 +1,29 @@
+import { Component } from "@angular/core";
+import { Router } from '@angular/router';
+
+@Component({
+    template:`
+
+        <h1>New Event</h1>
+        <hr>
+        <div class="col-md-6">
+            <br/>
+            <br/>
+            <button type="submit" class="btn btn-primary">Save</button>
+            <button type="button" class="btn btn-default" (click)="cancel()">Cancel</button>
+        </div>  
+    `
+})
+export class CreateEventComponent{
+
+    isDirty: boolean = true;
+    constructor(private route: Router){
+
+    }
+
+
+    cancel(){
+        this.route.navigate(['/events']);
+    }
+
+}
